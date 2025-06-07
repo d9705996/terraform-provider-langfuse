dbf08d54a6ed1162a7e7d22f20ed76f6e88cd350
David Walker <d9705996@users.noreply.github.com>
2025-06-07 14:33:45 +0100

    Add goreleaser workflow (#10)

--- main.go
+++ main.go
@@ -5,6 +5,15 @@ import (
 	"github.com/langfuse/terraform-provider-langfuse/langfuse"
 )
 
+var (
+	// version and commit are set by goreleaser at build time.
+	version = "dev"
+	commit  = ""
+)
+
 func main() {
-	plugin.Serve(&plugin.ServeOpts{ProviderFunc: langfuse.Provider})
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: langfuse.Provider,
+		ProviderAddr: "registry.terraform.io/langfuse/langfuse",
+	})
 }
